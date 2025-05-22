8f20c715a489128e83945f4e28e33ecb464b2deb
juanca <juandeabreu14@gmail.com>
2025-05-23 00:12:49 +0200

    routes update

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,11 +1,17 @@
 import pytest
-from app import create_app
+from app import create_app, db
 
 @pytest.fixture
 def client():
-    # Usamos el entorno 'testing' que utiliza una DB en memoria
+    # Crea la app con configuración de test
     app = create_app('testing')
     app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # DB temporal en memoria
+
+    with app.app_context():
+        db.create_all()  # ✅ Crea las tablas
+
+    # Devuelve el cliente de pruebas
     with app.test_client() as client:
         yield client
 
